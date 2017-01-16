de78e4bf069e794b3b5ee22f18886e224da78f6f
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-01-16 21:38:15 +0100

    added simple index route

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
-import ReduxPromise from 'redux-promise';
+import {Router, browserHistory} from 'react-router';
 
+import routes from './routes';
 import reducers from './reducers';
-import App from './components/App';
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+//const API_KEY = dankpepememesxddddd;
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(
       reducers,
       window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
-    <App />
+    <Router history={browserHistory} routes={routes} />
   </Provider>,
   document.getElementById('root')
 );
