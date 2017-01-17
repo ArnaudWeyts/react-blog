5fd63600e8ac95ad4c3b1557bca1ea93d0d22c4e
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-01-17 14:44:02 +0100

    added index route

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
+import promise from 'redux-promise';
 import {Router, browserHistory} from 'react-router';
 
 import routes from './routes';
 import reducers from './reducers';
 
-//const API_KEY = dankpepememesxddddd;
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(
