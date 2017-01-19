fc575a9bfc75b98e79af4b3f61fffb89acb5e981
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-01-19 16:42:29 +0100

    added createpost action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import 'whatwg-fetch';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=dankpepememesxddddd';
@@ -20,5 +21,18 @@ export function fetchPosts() {
   return {
     type: FETCH_POSTS,
     payload: request
-  }
+  };
+}
+
+export function createPost(props) {
+  const url = `${ROOT_URL}/posts${API_KEY}`;
+  const request = fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(props)
+  });
+
+  return {
+    type: CREATE_POST,
+    payload: request
+  };
 }
