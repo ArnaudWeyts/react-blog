1f07dd66fcf3344c8c075b73c8a8b6e69564af63
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-01-20 11:49:26 +0100

    split up form from the actual component

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -1,34 +1,16 @@
 import React, {Component} from 'react';
-import {Field, reduxForm} from 'redux-form';
-import {createPost} from '../actions/index';
+import PostsNewForm from './PostsNewForm';
+import SubmitButton from './SubmitButton';
 
 class PostsNew extends Component {
-  handleSubmit = (values) => {
-    console.log(values);
-  }
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <h3>Create a new post</h3>
-        <div className="form-group">
-          <label htmlFor="title">Title</label>
-          <Field name="title" component="input" type="text" className="form-control" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="categories">Categories</label>
-          <Field name="categories" component="input" type="text" className="form-control" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="content">Content</label>
-          <Field name="content" component="textarea" className="form-control" />
-        </div>
-
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
-    )
+      <div>
+        <PostsNewForm />
+        <SubmitButton />
+      </div>
+    );
   }
 }
 
-export default reduxForm({
-  form: 'PostsNewForm',
-}, null, {createPost})(PostsNew);
+export default PostsNew;
